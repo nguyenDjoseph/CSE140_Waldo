826b90a08eb45dd5e8e410034ef212d6e9acf4d1
samugibs <samugibs@ucsc.edu>
2020-02-08 18:43:25 -0800

    restructured the images directory

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+# CSE140: Wheres Waldo
+#
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Input
 
 
-waldoImages = 'train'
+waldoImages = 'train/64/containswaldo'
 list_Images = os.listdir(waldoImages)
 print(list_Images)
 
@@ -15,16 +17,20 @@ ncols = 2
 pic_index = 0
 
 fig = plt.gcf()
-fig.set_size_inches(ncols*4, nrows*4)
 
 pic_index +=4
-next_waldo_img = [os.path.join(waldoImages, fname) for fname in list_Images[pic_index-4:pic_index]]
+next_waldo_img = [os.path.join(waldoImages, fname) for fname in list_Images[1:2]]
 i = 0
 for img_path in next_waldo_img:
-    sp = plt.subplot(nrows, ncols, i+1)
-    sp.axis('off')
     img = mpimg.imread(img_path)
+#    input_layer = Input(shape=(img.size))
+#    x1 =  Dense(3, activation='relu')(input_layer)
+#    x2 = Dense(2, activation='relu')(x1)
+#    x3 = Dense(1, activation='sigmoid')(x2)
+    sp = plt.subplot()
+    sp.axis('off')
     plt.imshow(img)
+    
     i+=1
-plt.show()
+    plt.show()
     
